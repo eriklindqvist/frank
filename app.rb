348c8cc87298d44d3689dc3f66797e9782c207d8
Erik Lindqvist <erikjo82+github@gmail.com>
2016-11-29 22:13:43 +0100

    Add device status

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ end
 
 get '/devices' do
   content_type :json
-  out = `ssh pi@raspberry 'tdtool -l'`
+  out = `ssh pi@raspberry2 'tdtool -l'`
   num_devices = out.match(/Number of devices: ([0-9].)/)[1].to_i
   out.lines[1..num_devices].map { |l|
     cols = l.chomp.split("\t")
@@ -14,7 +14,12 @@ get '/devices' do
   }.to_json
 end
 
-get '/device/:id/:status' do
+post '/device/:id' do
+  out = `ssh pi@raspberry2 'tdtool --list-devices | grep -e "id=#{params[:id]}\\s"'`
+  out.split("=").last.downcase
+end
+
+post '/device' do
   if on? params[:status]
     status = 'n'
   elsif off? params[:status]
@@ -23,7 +28,7 @@ get '/device/:id/:status' do
     return [403, 'invalid status']
   end
 
-  `ssh pi@raspberry 'tdtool -#{status} #{params[:id]}'`
+  `ssh pi@raspberry2 'tdtool -#{status} #{params[:id]}'`
 end
 
 def on? str
